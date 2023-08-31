25b21aa1042a16ccc1fa170a1b9e3fa14d0c7d26
Luis Grangeiro <luiscarlos.grangeiro@gmail.com>
2023-08-31 01:41:49 -0300

    Add encriptação do password

--- lib/banana_bank/users/user.ex
+++ lib/banana_bank/users/user.ex
@@ -2,10 +2,11 @@ defmodule BananaBank.Users.User do
     use Ecto.Schema
     import Ecto.Changeset
 
-    @required_params [:name, :password_hash, :email, :cep]
+    @required_params [:name, :password, :email, :cep]
 
     schema "users" do
         field :name, :string
+        field :password, :string, virtual: true
         field :password_hash, :string
         field :email, :string
         field :cep, :string
@@ -18,5 +19,14 @@ defmodule BananaBank.Users.User do
         |> cast(params, @required_params)
         |> validate_required(@required_params)
         |> validate_length(:name, min: 3)
+        |> validate_format(:email, ~r/@/)
+        |> validate_length(:cep, is: 8)
+        |> add_password_hash()
     end
+
+    defp add_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+        change(changeset, Argon2.add_hash(password))
+    end
+
+    defp add_password_hash(changeset), do: changeset
 end
